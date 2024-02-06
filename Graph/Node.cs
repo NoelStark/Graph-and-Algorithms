0e17d3f17dd6f21f15f7a1b68193c38763c40d76
Noel Stark <noel.k.stark@gmail.com>
2024-02-06 12:20:03 +0100

    updated

--- Graph/Node.cs
+++ Graph/Node.cs
@@ -1,20 +1,21 @@
 ﻿
-namespace Graph
+
+using Graph;
+
+public class Node
 {
-    public class Node
+    public string Label;
+    public List<Edge> Neighbours { get; }
+
+    public Node(string label)
     {
-        public string Label;
-        public List<Edge> Neighbours { get; }
+        this.Label = label;
+        this.Neighbours = new List<Edge>();
+    }
 
-        public Node(string label)
-        {
-            this.Label = label;
-            this.Neighbours = new List<Edge>();
-        }
-
-        public void AddNeighbor(Node neighbour, int weight)
-        {
-            Neighbours.Add(new Edge(neighbour, weight));
-        }
+    public void AddNeighbor(Node neighbour, int weight)
+    {
+        Neighbours.Add(new Edge(neighbour, weight));
     }
 }
+
